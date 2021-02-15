699a042d64c0fc0ccbdd5e6cf7f40a5caa95da9f
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-02-15 13:49:24 +0900

    Add files via upload

---
+++ 2-1-2-2 Programming Design.c
@@ -0,0 +1,10 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 2 , Semester 1 , Part 2 , Chapter 2 */
+
+#include <stdio.h>
+
+void main()
+{
+	printf("Programming Design\n");
+	printf("ParkSooYoung\n");
+}
---
+++ 2-1-2-3 printf.c
@@ -0,0 +1,40 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 2 , Semester 1 , Part 2 , Chapter 3 */
+
+#include <stdio.h>
+
+void main()
+{
+	printf("Age : 24\n");
+	printf("Age : %d\n", 24);
+	printf("Output : 3 and 4\n");
+	printf("Output : %d and %d\n", 3, 4);
+	printf("Height : 174.3 cm\n");
+	printf("Height : %f cm\n", 174.3);
+	printf("Output : 3.4 and 4.3\n");
+	printf("Output : %f and %f\n", 3.4, 4.3);
+	printf("Output : %.1f and %.1f\n", 3.4, 4.4);
+
+	printf("\n");
+
+	printf("+ Calculate Result : 7\n");
+	printf("+ Calculate Result : %d\n", 3 + 4);
+	printf("3 + 4 = %d\n", 3 + 4);
+	printf("%d + %d = %d\n", 3, 4, 3 + 4);
+	printf("%d - %d = %d\n", 3, 4, 3 - 4);
+	printf("%d * %d = %d\n", 3, 4, 3 * 4);
+	printf("%d / %d = %f\n", 3, 4, 3. / 4.);
+
+	printf("\n");
+
+	printf("Grade : A\n");
+	printf("Grade : %c\n", 'A');
+	printf("Class A Average : 77\n");
+	printf("Class %c Average : %d\n", 'A', 77);
+	printf("Subject : Programming Design\n");
+	printf("Subject : %s\n", "Programming Design");
+	printf("First Name : Park\n");
+	printf("Last Name : SooYoung\n");
+	printf("First Name : %s\n", "Park");
+	printf("Last Name : %s\n", "SooYoung");
+}
