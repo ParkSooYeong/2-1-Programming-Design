194b2f808c6af3c1dfbd9d0caf4200c117f477b7
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-02-20 18:08:44 +0900

    Add files via upload

---
+++ 2-1-7-15 user.h
@@ -0,0 +1,6 @@
+struct users
+{
+	char name[20];
+	char phone[14];
+	int quick;
+};
---
+++ 2-1-7-16 User Defined Data Type.c
@@ -0,0 +1,65 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 2 , Semester 1 , Part 7 , Chapter 16 */
+
+#define _CRT_SECURE_NO_WARNINGS // Warning C4996 Error
+
+#include <stdio.h>
+#include <string.h>
+
+struct patient2
+{
+	int age;
+	unsigned sex : 1;
+	unsigned drinking : 1;
+	unsigned smoking : 1;
+	unsigned pressure : 2;
+};
+
+union uchr
+{
+	char chr1;
+	char chr2[3];
+};
+
+enum week { sun, mon, tue, wed, thu, fri, sat };
+enum prod { engine = 3, battery, filter, fuse };
+
+int main(void)
+{
+	union uchr data;
+	strcpy(data.chr2, "AB");
+	printf("data.chr1 = %c\n", data.chr1);
+	printf("data.chr2 = %s\n", data.chr2);
+	data.chr1 = 'C';
+	printf("data.chr1 = %c\n", data.chr1);
+	printf("data.chr2 = %s\n", data.chr2);
+
+	printf("\n");
+
+	enum week day1;
+	day1 = fri;
+	printf("day1 : %d\n", day1);
+	printf("fri : %d\n", fri);
+	day1 = mon;
+	printf("day1 : %d\n", day1);
+	printf("mon : %d\n", mon);
+
+	printf("\n");
+
+	enum prod v;
+	v = battery;
+	printf("장치번호 : %d\n", v);
+	printf("engine 번호 : %d\n", engine);
+	v = fuse;
+	printf("장치번호 : %d\n", v);
+
+	printf("\n");
+
+	struct patient2 p1 = { 24,0,1,0,3 };
+	printf("age : %d\n", p1.age);
+	printf("sex : %d\n", p1.sex);
+	printf("drinking : %d\n", p1.drinking);
+	printf("smoking : %d\n", p1.smoking);
+	printf("blood pressure : %d\n", p1.pressure);
+	return 0;
+}
